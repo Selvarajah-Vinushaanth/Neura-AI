bfc7a6b70d29e4393f707425a3d505eb4b7f67ff
Selvarajah Vinushaanth <selvavinu26816@gmail.com>
2025-03-13 11:41:00 +0530

    icon set

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,8 @@ const inter = Inter({ subsets: ["latin"] })
 export const metadata: Metadata = {
   title: "Neura AI Chat",
   description: "Chat with Neura AI - Text and Image Analysis",
-    generator: 'Vinushaanth'
+    generator: 'Vinushaanth',
+    icons: "/favicon.ico",
 }
 
 export default function RootLayout({
@@ -19,6 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <link rel="icon" href="/OIP (2).ico" />
       <body className={`${inter.className} antialiased`}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           {children}
